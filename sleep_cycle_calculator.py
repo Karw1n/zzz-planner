fa03ab642f903c0cc5333e3a3a23c08aa933504f
Karw1n <akbbalfour@gmail.com>
2025-07-15 18:19:17 +0100

    sleep_cycle_calculator function generates dictionary of sleep cycles added onto  sleep time.

--- sleep_cycle_calculator.py
+++ sleep_cycle_calculator.py
@@ -1,9 +1,12 @@
+from datetime import datetime, timedelta
 
 # bed_time: Float, the time of sleep in hours
+# Currently returns a list of sleep cycles and the total sleep time.
+# Constraint that needs to be added, 24 hour clock
 def sleep_cycle_calculator(bed_time):
-  time_to_fall_asleep = 15  # Average time for someone to fall asleep
-  sleep_cycle_time_min = 90     # Typcial sleep cycle time
-  sleep_cycle_time_hr = round((sleep_cycle_time_min / 60), 2)
+  time_to_fall_asleep = timedelta(minutes=15)  # Average time for someone to fall asleep
+  sleep_cycle_time = timedelta(minutes=90)     # Typical sleep cycle time
+  
   
   # The time the person falls asleep
   sleep_start_time = bed_time + time_to_fall_asleep
@@ -15,18 +18,19 @@ def sleep_cycle_calculator(bed_time):
   
   for i in range(sleep_cycle_range):
     cycle = i + 1
-    wake_time = (cycle * sleep_cycle_time_hr)
-    print(wake_time, cycle, sleep_cycle_time_hr)
+    wake_time = sleep_start_time + (cycle * sleep_cycle_time)
+    
     wake_times[cycle] = wake_time
   
   return wake_times
 
 
-my_bed_time = 9
+my_bed_time = datetime.strptime("21:00", "%H:%M")
 my_wake_time_options = sleep_cycle_calculator(my_bed_time)
 
 for sleep_cycle, wake_time in my_wake_time_options.items():
-  print(f'Sleep Cycle: {sleep_cycle}. Wake Up time: {wake_time}')
+  print(f'Number of Sleep Cycles: {sleep_cycle}. Wake Up time: {wake_time.strftime("%H:%M")}')
   
 
-  
+# In order to add these sleep times a helper function is needed to accomodate for the 24 hour clock
+# Returns the sum of two times on a 24 hour clock
