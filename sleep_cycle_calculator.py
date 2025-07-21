1cf3ef49547479c2ba8f6ade090a897204773491
Karw1n <akbbalfour@gmail.com>
2025-07-21 21:02:54 +0100

    Turned magic numbers into final variables.

--- sleep_cycle_calculator.py
+++ sleep_cycle_calculator.py
@@ -1,8 +1,13 @@
 from datetime import datetime, timedelta
 
+OPTIMUM_CYCLE           = 5 # 5 Cycles is recommended
+OPTIMUM_RECOVERY_CYCLE  = 6 # 6 Cycles is recommended for full recovery.
+AVG_TIME_TO_SLEEP_MIN   = 15 # Average time for someone to sleep
+SLEEP_CYCLE_TIME_MIN    = 90 # Typical sleep cycle time
+
 def end_of_sleep_cycle_wake_times(bed_time):
-  time_to_fall_asleep = timedelta(minutes=15)  # Average time for someone to fall asleep
-  sleep_cycle_time = timedelta(minutes=90)     # Typical sleep cycle time
+  time_to_fall_asleep = timedelta(minutes=AVG_TIME_TO_SLEEP_MIN)
+  sleep_cycle_time = timedelta(minutes=SLEEP_CYCLE_TIME_MIN)
   
   
   # The time the person falls asleep
