cb09229c5e0a5aed945e6bb3cc064a7d7d6b8835
Karw1n <akbbalfour@gmail.com>
2025-07-23 10:10:11 +0100

    Created a function to receive description based off number of cycles.

--- sleep_cycle_calculator.py
+++ sleep_cycle_calculator.py
@@ -1,9 +1,11 @@
 from datetime import datetime, timedelta
+import json
 
 OPTIMUM_CYCLE           = 5 # 5 Cycles is recommended
 OPTIMUM_RECOVERY_CYCLE  = 6 # 6 Cycles is recommended for full recovery.
 AVG_TIME_TO_SLEEP_MIN   = 15 # Average time for someone to sleep
 SLEEP_CYCLE_TIME_MIN    = 90 # Typical sleep cycle time
+JSON_FILE_LOCATOIN      = 'data/sleep_cycle_desc.json'
 
 
 def get_optimum_wake_up_time(sleep_time: datetime):
@@ -17,4 +19,13 @@ def get_time_to_fall_asleep():
 def get_sleep_cycle_time():
   return timedelta(minutes=SLEEP_CYCLE_TIME_MIN)
 
+def get_cycle_description(sleep_cycle_number):
+  with open(JSON_FILE_LOCATOIN) as file:
+    data = json.load(file)
+    
+    for item in data:
+      if item['number_of_cycles'] == sleep_cycle_number:
+        return item['description']
+      
+  return "Cycle number not found"
 
